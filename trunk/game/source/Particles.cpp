a506bb29d22239150697990abcc8b44a51c0eeb6
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@f18595e4-2f2c-44dc-29b7-6ac70ad1783f>
2011-10-19 15:37:12 +0000

    Adding Particle scripts in game.

---
+++ trunk/game/source/Particles.cpp
@@ -0,0 +1,32 @@
+/** 
+ * \file Particles.cpp
+ * \brief CParticles class is containing Particle effects.
+ * 
+ * \author Petar Bajic, MPE (C) All Rights Reserved, Homepage: www.mystic-peanut.com
+ * \date October, 21 2011.
+ */
+
+#include <irrlicht.h>
+using namespace irr;
+using namespace io;
+#include "Particles.h"
+#include "GameManager.h"
+
+/**
+ * \brief Standard constructor.
+ * \author Petar Bajic 
+ * \date October, 21 2011.
+ */
+CParticles::CParticles()
+{
+	
+}
+
+/**
+ * \brief Standard destructor.
+ * \author Petar Bajic 
+ * \date October, 21 2011.
+ */
+CParticles::~CParticles()
+{
+}
---
+++ trunk/game/source/Particles.h
@@ -0,0 +1,49 @@
+/** 
+ * \file Particles.h
+ * \brief CParticles class is containing Particle effects.
+ * 
+ * \author Petar Bajic, MPE (C) All Rights Reserved, Homepage: www.mystic-peanut.com
+ * \date October, 21 2011.
+ */
+
+#if !defined(AFX_ParticlesMANAGER_H__AB04DC05_BB05_11D4_87CB_00C04F73BBBB__INCLUDED_)
+#define AFX_ParticlesMANAGER_H__AB04DC05_BB05_11D4_87CB_00C04F73BBBB__INCLUDED_
+
+#include <irrlicht.h>
+using namespace irr;
+using namespace core;
+
+//! Particle effect kind.
+/** Particle effect type can be whirl, fire, explosion, etc... */
+enum PARTICLES_EFFECT_TYPE
+{
+	//!Particles circling in cylinder like a whirlwind
+	PARTICLES_EFFECT_WHIRL = 0,
+
+	//!Particles blasting from one point in all directions
+	PARTICLES_EFFECT_BLAST,
+
+	//!Particles slowly rising in magic field area
+	PARTICLES_EFFECT_FIELD,
+
+	//!Fire looking effect
+	PARTICLES_EFFECT_FIRE,
+
+	//!Smoke looking effect
+	PARTICLES_EFFECT_SMOKE
+};
+
+class CGameManager;
+
+class CParticles
+{
+public:
+
+	CParticles();
+	~CParticles();
+	
+	CGameManager* m_GameManager;
+
+};
+
+#endif;
